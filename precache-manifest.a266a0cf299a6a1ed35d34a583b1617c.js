a2d227afdd79e724463f9e0829879aaddc225f68
USER <user@USERs-MacBook-Pro.local>
2020-04-29 14:00:23 -0400

    Updates

---
+++ precache-manifest.a266a0cf299a6a1ed35d34a583b1617c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed23d8c7ffac263d0e58db919ae2df8d",
+    "url": "//deniro5.github.io/Pokedex2/index.html"
+  },
+  {
+    "revision": "45551e2ae5dd9da5cae7",
+    "url": "//deniro5.github.io/Pokedex2/static/css/main.7e42e6dd.chunk.css"
+  },
+  {
+    "revision": "1e9b3c64f9df2c07d6df",
+    "url": "//deniro5.github.io/Pokedex2/static/js/2.77d832ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//deniro5.github.io/Pokedex2/static/js/2.77d832ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45551e2ae5dd9da5cae7",
+    "url": "//deniro5.github.io/Pokedex2/static/js/main.0dad96b4.chunk.js"
+  },
+  {
+    "revision": "e0f7a8698792db3fd188",
+    "url": "//deniro5.github.io/Pokedex2/static/js/runtime-main.c2d5fc34.js"
+  }
+]);
